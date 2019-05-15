3c2f6ee6cfca89f0f009ce92390fb6dac2f0c0b6
CenIII <cenchuan2013@gmail.com>
2019-05-15 15:20:12 -0400

    ...

--- attention/model.py
+++ attention/model.py
@@ -58,7 +58,7 @@ class StructuredSelfAttention(torch.nn.Module):
 
         self.conv1 = nn.Conv2d(1, 2, (1, 1))#, padding=(1,0))
         self.relu = nn.ReLU()
-        self.conv2 = nn.Conv2d(2, 2, (1, 100))#, padding=(1,0))
+        self.conv2 = nn.Conv2d(1, 2, (1, 100))#, padding=(1,0))
 
         # before classification, two branch nets erase each other. 
 
@@ -124,9 +124,9 @@ class StructuredSelfAttention(torch.nn.Module):
 
         # add conv here
         # conv1
-        feats = self.relu(self.conv1(outputs.unsqueeze(1)))
+        # feats = self.relu(self.conv1(outputs.unsqueeze(1)))
         # conv2
-        self.heatmaps = self.conv2(feats) #torch.Size([512, 2, 200, 1])
+        self.heatmaps = self.conv2(outputs) #torch.Size([512, 2, 200, 1])
         # GAP
         logits = torch.mean(self.heatmaps,dim=2).squeeze()
         # linear # softmax
